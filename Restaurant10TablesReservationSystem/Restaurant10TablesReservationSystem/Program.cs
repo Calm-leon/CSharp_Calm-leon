dec2a47b9605520813712a3f8c68660ac2a2ebc0
Calm Leon <cami200025@gmail.com>
2023-05-07 02:25:49 -0500

    Se terminó las clases y el mini proyecto

--- Restaurant10TablesReservationSystem/Restaurant10TablesReservationSystem/Program.cs
+++ Restaurant10TablesReservationSystem/Restaurant10TablesReservationSystem/Program.cs
@@ -4,15 +4,20 @@
  * 3. El programa debe repetirse hasta que se registren las 10 personas
  */
 
-string[] userNames = new string[10] {"migue", "", "", "", "", "", "", "", "", ""};
+string[] userNames = new string[10] {"", "", "", "", "", "", "", "", "", ""};
 
-int arrayCurrentIndex = 1;
+int arrayCurrentIndex = 0;
 bool userType;
 
 Console.WriteLine("Welcome to the best restaurant in the world!");
-while (true)
+while (arrayCurrentIndex < 10)
 {
-    Console.WriteLine("Are you registered user? Write true, or write false to register");
+    /*if (arrayCurrentIndex == 10)
+    {
+        Console.WriteLine("The restaurant is full, try again the next year");
+        Environment.Exit(0);
+    }*/
+    Console.WriteLine("\n \nAre you registered user? Write true, or write false to register");
     userType = Convert.ToBoolean(Console.ReadLine());
 
     if (userType == true)
@@ -20,5 +25,35 @@ while (true)
         Console.WriteLine("Hello, you are a registered user, please enter your exact user name");
         string userToSearch = Console.ReadLine();
         Console.WriteLine("The user you searched is {0}", userToSearch);
+        int index = Array.IndexOf(userNames, userToSearch);
+
+        if (index == -1)
+        {
+            Console.WriteLine("User not found, try again or register");
+        }
+        else
+        {
+            Console.WriteLine("Welcome {0}, it's a pleasure to give your food", userToSearch);
+        }
+    }
+    else if (userType == false)
+    {
+        Console.WriteLine("Please write and remember your User Name");
+
+        userNames[arrayCurrentIndex] = Console.ReadLine();
+
+        Console.WriteLine("Your User has been saved successfully\n" + 
+                          "Your User Name is> {0}", userNames[arrayCurrentIndex]);
+        arrayCurrentIndex++;
     }
 }
+
+Console.WriteLine("The restaurant is full, try again the next year\n These are the guest to the dinner:");
+
+int auxIndex = 0;
+foreach (string element in userNames)
+{
+    Console.WriteLine("User number: {0} and user name: {1}", auxIndex + 1, userNames[auxIndex]);
+    auxIndex++;
+}
+Environment.Exit(0);
